dbc92ed76584ece1939bf129a8a6d067dec18367
MinYong Lee <twominyong@gmail.com>
2023-11-10 21:31:27 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 26.17 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
@@ -1,12 +1,8 @@
 class Solution {
     fun solution(arr: IntArray): IntArray {
         
-        var copyArr = arr.copyOf()
-        copyArr.sort()
-
-        if (copyArr.size == 1) { }
-
-        var answer = arr.filter { it > copyArr[0] }.toIntArray()
+        var answer = if (arr.size == 1) { arrayOf(-1).toIntArray() } 
+        else arr.filter { it > arr.minOrNull()!! }.toIntArray()
         
         return answer
     }
